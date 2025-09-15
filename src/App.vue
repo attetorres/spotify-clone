16cf6d8aef28b4aac144abd4cf92b84155732f1d
attetorres <atteneritorresgonzalez@gmail.com>
2025-09-15 19:18:42 +0100

    Create project, install dependencies and add buttons

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<script setup>
+import { ref, onMounted } from "vue";
+import { RouterLink, RouterView } from "vue-router";
+
+import ChevronUp from "vue-material-design-icons/ChevronUp.vue";
+import ChevronDown from "vue-material-design-icons/ChevronDown.vue";
+import ChevronRight from "vue-material-design-icons/ChevronRight.vue";
+import ChevronLeft from "vue-material-design-icons/ChevronLeft.vue";
+
+let openMenu = ref(false);
+</script>
+
+<template>
+  <div
+    class="w-[calc(100%-240px)] h-[60px] fixed right-0 z-20 bg-[#101010] opacity-80 flex items-center justify-between"
+  >
+    <div class="flex items-center ml-6">
+      <button
+        type="button"
+        class="rounded-full bg-black p-[1px] cursor-pointer"
+      >
+        <ChevronLeft fill-color="#FFFFFF" :size="30" />
+      </button>
+      <button
+        type="button"
+        class="rounded-full bg-black p-[1px] ml-4 cursor-pointer"
+      >
+        <ChevronRight fill-color="#FFFFFF" :size="30" />
+      </button>
+    </div>
+    <button
+      class="bg-black hover:bg-[#282828] rounded-full p-0.5 mr-8 mt-0.5 cursor-pointer"
+    >
+      <div class="flex items-center">
+        <img
+          class="rounded-full"
+          width="27"
+          src="https://pbs.twimg.com/profile_images/826365308923428864/Zb98Mxuz_400x400.jpg"
+        />
+        <div class="text-white text-[14px] ml-1.5 mr-1.5 font-semibold">
+          attetorres
+        </div>
+        <ChevronDown
+          v-if="!openMenu"
+          @click="openMenu = true"
+          fill-color="#FFFFFF"
+          :size="25"
+        />
+        <ChevronUp
+          v-else
+          @click="openMenu = false"
+          fill-color="#FFFFFF"
+          :size="25"
+        />
+      </div>
+    </button>
+  </div>
+</template>
