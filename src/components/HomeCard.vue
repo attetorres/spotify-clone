0fe87b73394a5396d37704f53ce842efb94b7647
attetorres <atteneritorresgonzalez@gmail.com>
2025-09-17 00:00:01 +0100

    Create HomeView and HomeCare components, add HomeCare elements to HomeView

---
+++ src/components/HomeCard.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { RouterLink } from "vue-router";
+
+const props = defineProps({
+  image: String,
+  title: String,
+  subTitle: String,
+});
+</script>
+
+<template>
+  <RouterLink to="library">
+    <div
+      class="bg-[111111] p-4 rounded-md m-2 hover:bg-[#252525] cursor-pointer"
+    >
+      <img class="rounded-md" :src="props.image" />
+      <div class="text-white pt-4 font-semibold text-[17px]">{{ title }}</div>
+      <div class="text-gray-400 pt-1 pb-3 text-[14px]">{{ subTitle }}</div>
+    </div>
+  </RouterLink>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,84 @@
+<script setup>
+import HomeCard from "../components/HomeCard.vue";
+</script>
+
+<template>
+  <div class="p-8">
+    <button
+      type="button"
+      class="text-white text-2xl font-semibold hover:underline cursor-pointer"
+    >
+      Podcast to make you smarter
+    </button>
+    <div class="py-1.5"></div>
+    <div class="flex items-center">
+      <HomeCard
+        image="https://picsum.photos/id/80/300/300"
+        title="Title is here"
+        subTitle="Subtitle is here"
+      />
+      <HomeCard
+        image="https://picsum.photos/id/38/300/300"
+        title="Title is here"
+        subTitle="Subtitle is here"
+      />
+      <HomeCard
+        class="md:block hidden"
+        image="https://picsum.photos/id/69/300/300"
+        title="Title is here"
+        subTitle="Subtitle is here"
+      />
+      <HomeCard
+        class="lg:block hidden"
+        image="https://picsum.photos/id/66/300/300"
+        title="Title is here"
+        subTitle="Subtitle is here"
+      />
+      <HomeCard
+        class="xl:block hidden"
+        image="https://picsum.photos/id/100/300/300"
+        title="Title is here"
+        subTitle="Subtitle is here"
+      />
+    </div>
+  </div>
+  <div class="p-8">
+    <button
+      type="button"
+      class="text-white text-2xl font-semibold hover:underline cursor-pointer"
+    >
+      Podcast to make you smarter
+    </button>
+    <div class="py-1.5"></div>
+    <div class="flex items-center">
+      <HomeCard
+        image="https://picsum.photos/id/123/300/300"
+        title="Title is here"
+        subTitle="Subtitle is here"
+      />
+      <HomeCard
+        image="https://picsum.photos/id/95/300/300"
+        title="Title is here"
+        subTitle="Subtitle is here"
+      />
+      <HomeCard
+        class="md:block hidden"
+        image="https://picsum.photos/id/83/300/300"
+        title="Title is here"
+        subTitle="Subtitle is here"
+      />
+      <HomeCard
+        class="lg:block hidden"
+        image="https://picsum.photos/id/230/300/300"
+        title="Title is here"
+        subTitle="Subtitle is here"
+      />
+      <HomeCard
+        class="xl:block hidden"
+        image="https://picsum.photos/id/67/300/300"
+        title="Title is here"
+        subTitle="Subtitle is here"
+      />
+    </div>
+  </div>
+</template>
