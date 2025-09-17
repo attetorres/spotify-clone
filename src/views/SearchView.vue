0579a0f68c8b8e7423e21d42c602204983d13838
attetorres <atteneritorresgonzalez@gmail.com>
2025-09-17 20:49:37 +0100

    Create SearchView and CategorySelect component. Display data obtained from a created JSON of categories on SearchView.

---
+++ src/views/SearchView.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref } from "vue";
+import categoriesData from "../assets/Categories_Data.json";
+import CategorySelect from "../components/CategorySelect.vue";
+
+const categories = ref(categoriesData);
+</script>
+
+<template>
+  <div class="p-8">
+    <button
+      type="button"
+      class="text-white text-2xl font-semibold hover:underline cursor-pointer"
+    >
+      Podcast to make you smarter
+    </button>
+    <div class="py-1.5"></div>
+    <div
+      class="grid xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 sm: grid-cols-2 gap-6"
+    >
+      <CategorySelect
+        v-for="(item, index) in categories"
+        :key="index"
+        :category="item.category"
+        :image="item.image"
+      />
+    </div>
+  </div>
+</template>
